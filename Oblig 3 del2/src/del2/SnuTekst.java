0da92c676a07a58c8d26d73fd1330112e9f51a13
Marius <Marius@158.39.124.233>
2015-03-05 17:30:19 +0100

    V 0.2

--- Oblig 3 del2/src/del2/SnuTekst.java
+++ Oblig 3 del2/src/del2/SnuTekst.java
@@ -1,10 +1,22 @@
 package del2;
 
+import java.util.Scanner;
+
 public class SnuTekst {
-
+	static int bokstavTeller;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		bokstavTeller = 0;
+		System.out.println("Skriv inn teksten du vil reversere: ");
+		System.out.println(baklengs(input.nextLine()));
 
+		input.close();
 	}
-
+	public static String baklengs(String tekst){
+		bokstavTeller++;
+		System.out.println("Antall tegn behandlet: " + bokstavTeller);
+		if ((tekst == null) || (tekst.length()<=1))
+			return tekst;
+		return (baklengs(tekst.substring(1)) + tekst.charAt(0));
+	}
 }
